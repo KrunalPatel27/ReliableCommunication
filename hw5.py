22197f2d96cea8a7436ddfb2e8f160f593ca4a6a
KrunalPatel27 <kpate236@gmail.com>
2018-04-18 15:27:09 -0500

    intitial commit

---
+++ hw5.py
@@ -0,0 +1,79 @@
+"""
+Where solution code to HW5 should be written.  No other files should
+be modified.
+"""
+
+import socket
+import io
+import time
+import typing
+import struct
+import homework5
+import homework5.logging
+
+
+def send(sock: socket.socket, data: bytes):
+    """
+    Implementation of the sending logic for sending data over a slow,
+    lossy, constrained network.
+
+    Args:
+        sock -- A socket object, constructed and initialized to communicate
+                over a simulated lossy network.
+        data -- A bytes object, containing the data to send over the network.
+    """
+
+    # Naive implementation where we chunk the data to be sent into
+    # packets as large as the network will allow, and then send them
+    # over the network, pausing half a second between sends to let the
+    # network "rest" :)
+
+    timeoutInterval = 1.0
+    headerSize = 4
+    logger = homework5.logging.get_logger("hw5-sender")
+    chunk_size = homework5.MAX_PACKET - headerSize
+    pause = 2.0
+    offsets = range(0, len(data), chunk_size)
+    for chunk in [data[i:i + chunk_size] for i in offsets]:
+
+        sock.send(chunk)
+        logger.info("Pausing for %f seconds", round(pause, 2))
+        sock.settimeout(pause)
+        #time.sleep(pause)
+        while true:
+            try:
+                data = sock.recv(headerSize)
+                if data:
+            except:
+                sock.send(chunk)
+                
+
+
+        
+
+
+def recv(sock: socket.socket, dest: io.BufferedIOBase) -> int:
+    """
+    Implementation of the receiving logic for receiving data over a slow,
+    lossy, constrained network.
+
+    Args:
+        sock -- A socket object, constructed and initialized to communicate
+                over a simulated lossy network.
+
+    Return:
+        The number of bytes written to the destination.
+    """
+    logger = homework5.logging.get_logger("hw5-receiver")
+    # Naive solution, where we continually read data off the socket
+    # until we don't receive any more data, and then return.
+    num_bytes = 0
+    while True:
+        data = sock.recv(homework5.MAX_PACKET)
+        if not data:
+            break
+        logger.info("Received %d bytes", len(data))
+        dest.write(data)
+        num_bytes += len(data)
+        dest.flush()
+    return num_bytes
